e5fab750b264f84de4bc6520b4e9c93b5cc5e409
Dinesh Subramanian <dineshsubramanian@dineshs-mbp-2.lan>
2024-06-06 17:27:20 +0530

    gateway routing

--- src/main/java/com/microcrafts/gateway/ApiGatewayConfiguration.java
+++ src/main/java/com/microcrafts/gateway/ApiGatewayConfiguration.java
@@ -1,18 +1,24 @@
 package com.microcrafts.gateway;
 
-import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.rewritePath;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+
 
 @Configuration
 public class ApiGatewayConfiguration {
 
     @Bean
-    public RouterFunction routeLocator() {
-        return RouterFunctions.route(RequestPredicates.path("/v1/albums"), HandlerFunctions.http("http://ec2-54-83-140-26.compute-1.amazonaws.com:8080/albums"));
+    public RouterFunction<ServerResponse> routeLocator() {
+        return route("albumsListRoute").GET(path("/api/v1/**"), http("http://ec2-54-83-140-26.compute-1.amazonaws.com:8080"))
+                .before(rewritePath("/api/v1/(?<segment>.*)","/${segment}")).build();
     }
 
 }
